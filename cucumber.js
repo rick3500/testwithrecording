7ff259674e4ea319189a84806a7580636e580fc2
Rick Simmons <rick3500@gmail.com>
2023-04-15 12:11:13 -0400

    cleaned up

---
+++ cucumber.js
@@ -0,0 +1,32 @@
+const cucumberHtmlReporter = require('cucumber-html-reporter');
+const { mkdirSync } = require('fs');
+const { join } = require('path');
+
+const reportsDirectory = join(process.cwd(), 'reports');
+
+if (!fs.existsSync(reportsDirectory)) {
+  mkdirSync(reportsDirectory);
+}
+
+exports.config = {
+  specs: ['./test/features/**/*.feature'],
+  cucumberOpts: {
+    require: ['./test/step_definitions/**/*.js'],
+    format: ['json:reports/cucumber.json', 'html:reports/cucumber.html'],
+    strict: true
+  },
+  onPrepare: () => {
+    console.log('Running Cucumber with Protractor');
+  },
+  onComplete: () => {
+    const options = {
+      theme: 'bootstrap',
+      jsonFile: 'reports/cucumber.json',
+      output: 'reports/cucumber.html',
+      reportSuiteAsScenarios: true,
+      launchReport: true,
+    };
+
+    cucumberHtmlReporter.generate(options);
+  }
+};
